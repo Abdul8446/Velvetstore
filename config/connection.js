ace5ea6813012c1845f6890f2844a1cecf7eb279
Abdul8446 <farhanfarhan832@gmail.com>
2022-12-14 22:59:48 +0530

    category correction in admin side

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect = function (done) {
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://velvetstore:lceQfQJ1SCBrD2fj@cluster0.dxv6kr8.mongodb.net/test'
     const dbname = 'shopping'
 
     mongoClient.connect(url, (err, data) => {
@@ -15,5 +15,5 @@ module.exports.connect = function (done) {
 }
 
 module.exports.get = function () {
-    return state.db
+    return state.db    
 }
